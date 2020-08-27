92147ed2ebfe614837bb02e2b364f2a138bf3b69
sakalaka8 <43861607+sakalaka8@users.noreply.github.com>
2020-08-27 21:09:37 +0200

    Added a monthly sum

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+'''
+    main.py program for app Workhours_tracker
+    Author: sakalaka8 (https://github.com/sakalaka8)
+'''
+
+# Import libraries
 import datetime
 import csv
 import os.path
 
+# Defines
 file_exists = os.path.isfile('workhours.csv')
 date_time = datetime.datetime.now()
-
 date = date_time.strftime("%d.%m.%Y")
 time = t = date_time.strftime("%H:%M:%S")
 day = date_time.strftime("%A")
 week = date_time.strftime("%U")
+
 '''
 print("Date:", date)
 print("Time:", time)
@@ -16,16 +23,25 @@ print("Day:", day)
 print("Week:", week)
 '''
 print("--- Workhours tracker ---")
-hours = input("Insert hours: ")
-print("Today you work "+ hours + " hours.")
+workhours = input("Insert hours: ")
+print("Today you work "+ workhours + " hours. Great!")
 
+# Add fieldnames to csv file
 with open('workhours.csv', '+a', newline='') as file:
     fieldnames = ["DATE", "DAY", "WEEK", "HOURS"]
     field_writer = csv.DictWriter(file, fieldnames = fieldnames)
     if not file_exists:
         field_writer.writeheader()
 
+# Write data to csv file
 with open('workhours.csv', 'a+', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow([date, day, week, hours])
+    writer.writerow([date, day, week, workhours])
     
+with open('workhours.csv', 'r', newline='') as file:
+    reader = csv.DictReader(file, delimiter=',')
+    monthly_sum = 0
+    for row in reader:
+        #print(row['HOURS'])
+        monthly_sum += float(row['HOURS'])
+    print("Monthly sum: "+str(monthly_sum))
